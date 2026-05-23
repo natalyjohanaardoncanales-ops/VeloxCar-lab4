59730b613117e57732f83ebaad70866f2606f933
natalyjohanaardoncanales-ops <natalyjohanaardoncanales@gmail.com>
2026-05-22 23:56:05 -0600

    lab4 inicial

---
+++ src/main/java/com/VeloxCar/VeloxCarApplication.java
@@ -0,0 +1,13 @@
+package com.VeloxCar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeloxCarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeloxCarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/VeloxCar/VeloxCarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.VeloxCar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeloxCarApplicationTests {
+
+	@Test
+	void contextLoads() { 
+	}
+
+}
