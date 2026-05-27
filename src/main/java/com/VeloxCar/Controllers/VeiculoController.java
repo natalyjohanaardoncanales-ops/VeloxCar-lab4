e8e4a657305647beba775f1370a4466e521f6719
natalyjohanaardoncanales-ops <natalyjohanaardoncanales@gmail.com>
2026-05-27 08:50:23 -0600

    añadir el html y la imagen de prueva

--- src/main/java/com/VeloxCar/Controllers/VeiculoController.java
+++ src/main/java/com/VeloxCar/Controllers/VeiculoController.java
@@ -1,12 +1,12 @@
 package com.VeloxCar.Controllers;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import com.VeloxCar.models.Veiculo;
 import com.VeloxCar.service.IVeiculoService;
 
@@ -25,5 +25,18 @@ public class VeiculoController {
 
         return "listado";
     }
+    @GetMapping("/veiculos/create")
+    public String crear(Veiculo veiculo) {
+        return "formVeiculo";
+    }
+    @PostMapping("/veiculos/save")
+    public String guardar(Veiculo veiculo, RedirectAttributes attributes) {
 
+        servicVeiculo.guardar(veiculo);
+
+        attributes.addFlashAttribute("msg", "Vehículo guardado correctamente");
+
+        return "redirect:/veiculos/index";
+    }
+    
 }
--- src/main/java/com/VeloxCar/models/Veiculo.java
+++ src/main/java/com/VeloxCar/models/Veiculo.java
@@ -18,6 +18,7 @@ public class Veiculo {
     private String modelo;
     private Double precio;
     private String descripcion;
+    private String imagen;
 
     public Integer getId() {
         return id;
@@ -57,5 +58,11 @@ public class Veiculo {
 
     public void setDescripcion(String descripcion) {
         this.descripcion = descripcion;
+    }
+    public String getImagen() {
+        return imagen;
+    }
+    public void setImagen(String imagen) {
+        this.imagen = imagen;
     }
 }
